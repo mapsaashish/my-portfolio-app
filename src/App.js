fd600f350684e9dd010c96df8eddca494cd19cd5
Aashish Sharma <mapsaashish@gmail.com>
2025-06-30 10:49:08 +0545

    Update profile image URL in App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const profileData = {
         email: 'itsaabhaas@gmail.com',
         dob: '18-01-1997',
         nationality: 'Nepali',
-        profileImage: 'https://github.com/mapsaashish/my-portfolio-app/blob/main/src/data/images/profile_photo.jpg' // Placeholder for profile image
+        profileImage: 'public\images\profile_photo.jpg' // Placeholder for profile image
     },
     academic: [
         {
